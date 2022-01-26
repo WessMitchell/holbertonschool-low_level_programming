57a1ac0d8f85b60b1a8d8b1f95998050269354e7
WessMitchell <4218@holbertonschool.com>
2022-01-26 07:37:46 -0800

    task 5 edit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
-*main prints the below statement
-*Return responds with 0 if successful
+*main - prints the below statement
+*Return (0) -  responds with 0 if successful
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
