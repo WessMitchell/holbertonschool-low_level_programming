ebaf7e029f5c0a7faed38e8605d3041d38af843e
WessMitchell <4218@holbertonschool.com>
2022-01-25 17:03:27 -0800

    task 4 edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
-
-
 #include <stdio.h>
 
+
+/**
+ * main - The starting point for the script
+ *
+ * Return: 0 - returns an integer 0 to int main
+ */
 int main(void)
 {
-
-  puts("\"Programming is like building a multilingual puzzle\n"); // Shows the words //
-  return(0);
+   puts("\"Programming is like building a multilingual puzzle\n"
+);
+   return(0);
 }
