423c88a5d24dec36b8aa3a1df9d16fcedba78d97
WessMitchell <4218@holbertonschool.com>
2022-01-26 08:08:57 -0800

    task 6 edit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,13 @@
 */
 int main(void)
 {
+int Type;
+float floatType;
+double doubleType;
+char charType;
 printf("Size of int: %zu bytes\n", sizeof(intType));
 printf("Size of float: %zu bytes\n", sizeof(floatType));
 printf("Size of double: %zu bytes\n", sizeof(doubleType));
 printf("Size of char: %zu byte\n", sizeof(charType));
-Return (0);
-} 
+Return(0);
+}
