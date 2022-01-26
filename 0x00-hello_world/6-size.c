2a639e0ba243117d4b29cc8c781e244cf758c44c
WessMitchell <4218@holbertonschool.com>
2022-01-26 08:32:08 -0800

    task 6 edit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,13 @@
 */
 int main(void)
 {
-printf("Size of int: %d bytes\n", sizeof(int));
-printf("Size of float: %d bytes\n", sizeof(float));
-printf("Size of double: %d bytes\n", sizeof(double));
-printf("Size of char: %d byte\n", sizeof(char));
+int;
+float;
+double;
+char;
+printf("Size of int: %d byte(s)\n", sizeof(int));
+printf("Size of float: %d byte(s)\n", sizeof(float));
+printf("Size of double: %d byte(s)\n", sizeof(double));
+printf("Size of char: %d byte(s)\n", sizeof(char));
 Return(0);
 }
